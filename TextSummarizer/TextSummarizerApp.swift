99a9f97719abc0826bb9848f2cfccfe8535a9f06
Gagik <gagik.navasatariyan@springernature.com>
2026-09-10 21:23:56 +0200

    Initial Commit

---
+++ TextSummarizer/TextSummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextSummarizerApp.swift
+//  TextSummarizer
+//
+//  Created by Gagik on 10/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct TextSummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
